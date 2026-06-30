b6f22cd388357d943051a366b92f941efcf6345f
alepsmn <alepsmn@gmail.com>
2026-06-30 13:17:26 +0000

    requirements listo

--- info_epri.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-wb = openpyxl.load_workbook("data/raw/epri/EPRI_2024_Projections.xlsx", read_only=True)
-print(wb.sheetnames)
-
-ws = wb.active
-for i, row in enumerate(ws.iter_rows(values_only=True)):
-    print(row)
-    if i >= 5:
-        break
